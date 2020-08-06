43073441c32355da33bfb35d9df15268433f0bed
한정훈 <onooff@naver.com>
2020-08-06 14:39:03 +0900

    WIP:에디터

--- frontend/src/page/post/Write.vue
+++ frontend/src/page/post/Write.vue
@@ -3,43 +3,35 @@
     <h1 class="text-center">글 쓰기</h1>
     <div class="form-group">
       <label for="exampleFormControlInput1" class="mb-3">글 제목</label>
-      <input
-        type="text"
-        class="form-control"
-        v-model="writeData.title"
-        id="exampleFormControlInput1"
-      />
+      <input type="text" class="form-control" v-model="writeData.title" id="exampleFormControlInput1" />
     </div>
     <div class="form-group">
       <label for="exampleFormControlTextarea1" class="mb-3">글 내용</label>
-      <textarea
-        class="form-control"
-        id="exampleFormControlTextarea1"
-        v-model="writeData.content"
-        rows="3"
-      ></textarea>
+      <textarea class="form-control" id="sample" v-model="writeData.content"></textarea>
+      <!-- <SunEditor id="exampleFormControlTextarea1" v-model="writeData.content"></SunEditor> -->
     </div>
     <br />
     <button @click="writeBoard" class="right btn btn-primary">확인</button>
-    <SunEditor></SunEditor>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import SunEditor from '../../page/post/Editor.vue'
+import "suneditor/dist/css/suneditor.min.css";
+import suneditor from "suneditor";
+import plugins from "suneditor/src/plugins";
 
 const BASE_URL = "http://127.0.0.1:8080";
 
 export default {
   name: "Write",
-   components : {
-    SunEditor
-  },
+  // components: {
+  //   SunEditor,
+  // },
   data() {
     return {
       writeData: {
-        content: null,
+        content: "",
         title: null,
         uid: null,
       },
@@ -57,12 +49,34 @@ export default {
           },
         })
         .then(() => {
-          this.$router.push("/#/"); 
+          this.$router.push("/#/");
         })
         .catch((err) => {
           console.log(err.response);
         });
     },
+  },
+  mounted() {
+    suneditor.create("sample", {
+      width: "80%",
+      height: "500px",
+      plugins: plugins,
+      buttonList: [
+        ["undo", "redo"],
+        ["font", "fontSize", "formatBlock"],
+        ["paragraphStyle", "blockquote"],
+        ["bold", "underline", "italic", "strike", "subscript", "superscript"],
+        ["fontColor", "hiliteColor", "textStyle"],
+        ["removeFormat"],
+        "/", // Line break
+        ["outdent", "indent"],
+        ["align", "horizontalRule", "list", "lineHeight"],
+        ["table", "link", "image", "video", "audio" /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin. // You must add the "imageGalleryUrl".
+        /** ['imageGallery'] */ ["fullScreen", "showBlocks", "codeView"],
+        ["preview", "print"],
+        ["save", "template"],
+      ],
+    });
   },
 };
 </script>
