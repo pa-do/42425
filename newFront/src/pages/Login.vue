c34b50dc1aba4056754920dff6392729dc5e7da9
박도희 <dohee.pa@gmail.com>
2020-08-07 16:32:52 +0900

    plz

--- newFront/src/pages/Login.vue
+++ newFront/src/pages/Login.vue
@@ -101,7 +101,6 @@ export default {
           this.$session.set("user", this.user);
           this.$router.push("/#/");
           this.$router.go();
-          // asdf
           this.$cookie.set("auth-token", this.user.uid, 1);
           alert(this.user.nickname + "님 환영합니다!");
         })
